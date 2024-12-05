718db0120fad26795a5e20b9044449805c90ead3
JelAnt <ante.jelavi12@gmail.com>
2024-12-05 21:06:13 +0000

    Added fast tests

---
+++ src/test/java/atulab/ie/mysteryitem/FastTests.java
@@ -0,0 +1,10 @@
+package atulab.ie.mysteryitem;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ AppTest.class, MysteryItemTest.class })
+public class FastTests {
+
+}
