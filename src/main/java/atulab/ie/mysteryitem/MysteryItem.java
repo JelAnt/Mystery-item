334595f5b8a051a68ae9be04b38d0055698ca50b
JelAnt <ante.jelavi12@gmail.com>
2024-12-06 18:28:01 +0000

    Feature: #4 added backstory and description of mystery items

--- src/main/java/atulab/ie/mysteryitem/MysteryItem.java
+++ src/main/java/atulab/ie/mysteryitem/MysteryItem.java
@@ -1,34 +1,65 @@
-/**
- * MysteryItem.java
- */
 package atulab.ie.mysteryitem;
 
 /**
  * MysteryItem class
- * MysteryItem class Holds item attributes
+ * This class holds attributes for a mystery item
+ * 
  * 
  * @author Ante Jelavic
  */
 public class MysteryItem {
-	private String name;
+    private String name;
+    private String lore;  // Added lore attribute to describe each item
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * Constructor to initialize a mystery item with a name and lore.
+     * 
+     * @param name the name of the mystery item
+     * @param lore the lore of the mystery item
+     */
+    public MysteryItem(String name, String lore) {
+        this.name = name;
+        this.lore = lore;
+    }
 
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * Gets the name of the mystery item.
+     * 
+     * @return the name of the mystery item
+     */
+    public String getName() {
+        return name;
+    }
 
-	@Override
-	public String toString() {
-		return "MysteryItem [name=" + name + "]";
-	}
+    /**
+     * Sets the name of the mystery item.
+     * 
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    /**
+     * Gets the lore of the mystery item.
+     * 
+     * @return the lore of the mystery item
+     */
+    public String getLore() {
+        return lore;
+    }
+
+    /**
+     * Sets the lore of the mystery item.
+     * 
+     * @param lore the lore to set
+     */
+    public void setLore(String lore) {
+        this.lore = lore;
+    }
+
+    @Override
+    public String toString() {
+        return "MysteryItem [name=" + name + ", lore=" + lore + "]";
+    }
 }
